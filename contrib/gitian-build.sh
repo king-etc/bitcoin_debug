00539727e41a48337bd5a9a2038bb43787966935
king-etc <K-Gollum@hotmail.com>
2018-10-11 14:06:34 +0800

    v1.2

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -38,7 +38,7 @@ version		Version number, commit, or branch to build. If building a commit or bra
 
 Options:
 -c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/bitcoin/bitcoin
+-u|--url	Specify the URL of the repository. Default is https://github.com/king-etc/bitcoin_debug
 -v|--verify 	Verify the Gitian build
 -b|--build	Do a Gitian build
 -s|--sign	Make signed binaries for Windows and Mac OSX
